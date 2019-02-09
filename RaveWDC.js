7945a186bea7cd5eb52158d77c3096bf9fe8c34b
Patrick Zhang <patrick.zhang@ocado.com>
2019-02-09 10:12:49 +0000

    corrected the indentation

--- RaveWDC.js
+++ RaveWDC.js
@@ -1,57 +1,56 @@
 (function () {
     var myConnector = tableau.makeConnector();
 
-myConnector.getSchema = function (schemaCallback) {
-    var cols = [{
-        id: "id",
-        dataType: tableau.dataTypeEnum.string
-    }, {
-        id: "mag",
-        alias: "magnitude",
-        dataType: tableau.dataTypeEnum.float
-    }, {
-        id: "title",
-        alias: "title",
-        dataType: tableau.dataTypeEnum.string
-    }, {
-        id: "location",
-        dataType: tableau.dataTypeEnum.geometry
-    }];
+    myConnector.getSchema = function (schemaCallback) {
+        var cols = [{
+            id: "userid",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "projectid",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "project",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "studyid",
+            dataType: tableau.dataTypeEnum.string
+        }];
 
-    var tableSchema = {
-        id: "RaveFeed",
-        alias: "Study BUP14-CN-301",
-        columns: cols
+        var tableSchema = {
+            id: "RaveFeed",
+            alias: "Study BUP14-CN-301",
+            columns: cols
+        };
+
+        schemaCallback([tableSchema]);
     };
 
-    schemaCallback([tableSchema]);
-};
-myConnector.getData = function(table, doneCallback) {
-    $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
-        var feat = resp.features,
-            tableData = [];
+    myConnector.getData = function(table, doneCallback) {
+        $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
+            var feat = resp.features,
+                tableData = [];
 
-        // Iterate over the JSON object
-        for (var i = 0, len = feat.length; i < len; i++) {
-            tableData.push({
-                "id": feat[i].id,
-                "mag": feat[i].properties.mag,
-                "title": feat[i].properties.title,
-                "location": feat[i].geometry
-            });
-        }
+            // Iterate over the JSON object
+            for (var i = 0, len = feat.length; i < len; i++) {
+                tableData.push({
+                    "id": feat[i].id,
+                    "mag": feat[i].properties.mag,
+                    "title": feat[i].properties.title,
+                    "location": feat[i].geometry
+                });
+            }
 
-        table.appendRows(tableData);
-        doneCallback();
-    });
-};
+            table.appendRows(tableData);
+            doneCallback();
+        });
+    };
 
     tableau.registerConnector(myConnector);
-$(document).ready(function () {
-    $("#submitButton").click(function () {
-        tableau.connectionName = "Rave EDC Feed";
-        tableau.submit();
+    $(document).ready(function () {
+        $("#submitButton").click(function () {
+            tableau.connectionName = "Rave EDC Feed";
+            tableau.submit();
+        });
     });
-});
 })();
 
